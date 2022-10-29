dcee3b450ec79a1470dc36a7fc4f98379daae614
Shridhar <shridhar.p@zerodha.com>
2022-10-29 16:15:44 +0530

    feat: display template in notification

--- frappe_whatsapp/frappe_whatsapp/doctype/whatsapp_notification/whatsapp_notification.js
+++ frappe_whatsapp/frappe_whatsapp/doctype/whatsapp_notification/whatsapp_notification.js
@@ -2,7 +2,23 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('WhatsApp Notification', {
-	// refresh: function(frm) {
-
-	// }
+	refresh: function(frm) {
+		frm.trigger("load_template")
+	},
+	template: function(frm){
+		frm.trigger("load_template")
+	},
+	load_template: function(frm){
+		frappe.db.get_value(
+			"WhatsApp Templates",
+			frm.doc.template,
+			"template",
+			(r) => {
+				if (r && r.template) {
+					frm.set_value("code", r.template);
+					frm.refresh_field("code")
+				}
+			}
+		)
+	}
 });
