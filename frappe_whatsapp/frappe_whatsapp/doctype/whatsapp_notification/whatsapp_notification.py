a871736e6dc5f0ad102e245c54c474ddefa79c1d
Shridhar <shridhar.p@zerodha.com>
2022-10-30 14:20:39 +0530

    feat: send messages using scheduled events

--- frappe_whatsapp/frappe_whatsapp/doctype/whatsapp_notification/whatsapp_notification.py
+++ frappe_whatsapp/frappe_whatsapp/doctype/whatsapp_notification/whatsapp_notification.py
@@ -3,17 +3,45 @@
 import json
 import frappe
 from frappe.model.document import Document
-from frappe.utils.safe_exec import get_safe_globals
+from frappe.utils.safe_exec import get_safe_globals, safe_exec
 from frappe.integrations.utils import make_post_request
 
 
 class WhatsAppNotification(Document):
     """Notification."""
     def validate(self):
+        if self.notification_type == "DocType Event":
+            if not any(field.fieldname == self.field_name for field in frappe.get_doc("DocType", self.reference_doctype).fields): # noqa
+                frappe.throw(f"Field name {self.field_name} does not exists")
 
-        if not any(field.fieldname == self.field_name for field in frappe.get_doc("DocType", self.reference_doctype).fields): # noqa
-            frappe.throw(f"Field name {self.field_name} does not exists")
 
+    def execute_method(self) -> dict:
+        """Specific to API endpoint Server Scripts."""
+
+        safe_exec(
+            self.condition, get_safe_globals(), dict(doc=self)
+        )
+        language_code = frappe.db.get_value(
+            "WhatsApp Templates", self.template,
+            fieldname='language_code'
+        )
+        if language_code:
+            for contact in self._contact_list:
+                data = {
+                    "messaging_product": "whatsapp",
+                    "to": contact,
+                    "type": "template",
+                    "template": {
+                        "name": self.template,
+                        "language": {
+                            "code": language_code
+                        },
+                        "components":[]
+                    }
+                }
+
+                self.notify(data)
+        # return _globals.frappe.flags
 
     def execute_doc(self, doc: Document):
         """Specific to Document Event triggered Server Scripts."""
@@ -28,15 +56,6 @@ class WhatsAppNotification(Document):
             ):
                 return
 
-        settings = frappe.get_doc(
-            "WhatsApp Settings", "WhatsApp Settings",
-        )
-        token = settings.get_password("token")
-
-        headers = {
-            "authorization": f"Bearer {token}",
-            "content-type": "application/json"
-        }
 
         language_code = frappe.db.get_value(
             "WhatsApp Templates", self.template,
@@ -72,13 +91,45 @@ class WhatsAppNotification(Document):
                     "parameters": parameters
                 }]
 
-            response = make_post_request(
-                f"{settings.url}/{settings.version}/{settings.phone_id}/messages",
-                headers=headers, data=json.dumps(data)
+            self.notify(data)
+
+    def notify(self, data):
+        """Notify."""
+        settings = frappe.get_doc(
+            "WhatsApp Settings", "WhatsApp Settings",
+        )
+        token = settings.get_password("token")
+
+        headers = {
+            "authorization": f"Bearer {token}",
+            "content-type": "application/json"
+        }
+
+        response = make_post_request(
+            f"{settings.url}/{settings.version}/{settings.phone_id}/messages",
+            headers=headers, data=json.dumps(data)
+        )
+
+        frappe.get_doc({
+            "doctype": "WhatsApp Notification Log",
+            "template": self.template,
+            "meta_data": response
+        }).insert(ignore_permissions=True)
+
+
+    def on_trash(self):
+        if self.notification_type == "Scheduler Event":
+            frappe.delete_doc("Scheduled Job Type", self.name)
+
+    def after_insert(self):
+        if self.notification_type == "Scheduler Event":
+            method = f"frappe_whatsapp.utils.trigger_whatsapp_notifications_{self.event_frequency.lower().replace(' ', '_')}" # noqa
+            job = frappe.get_doc(
+                {
+                    "doctype": "Scheduled Job Type",
+                    "method": method,
+                    "frequency": self.event_frequency
+                }
             )
 
-            frappe.get_doc({
-                "doctype": "WhatsApp Notification Log",
-                "template": self.template,
-                "meta_data": response
-            }).insert(ignore_permissions=True)
+            job.insert()
--- frappe_whatsapp/utils/__init__.py
+++ frappe_whatsapp/utils/__init__.py
@@ -4,10 +4,6 @@ import frappe
 from frappe.core.doctype.server_script.server_script_utils import EVENT_MAP
 
 
-def ping():
-    return "pong"
-
-
 def run_server_script_for_doc_event(doc, event):
     """Run on each event."""
     if event not in EVENT_MAP:
@@ -25,7 +21,6 @@ def run_server_script_for_doc_event(doc, event):
     if notification:
         # run all scripts for this doctype + event
         for notification_name in notification:
-            print(notification_name)
             frappe.get_doc("WhatsApp Notification", notification_name).execute_doc(doc)
 
 
@@ -51,3 +46,62 @@ def get_notifications_map():
         frappe.cache().set_value("whatsapp_notification_map", notification_map)
 
     return notification_map
+
+
+def trigger_whatsapp_notifications_all():
+    """Run all."""
+    trigger_whatsapp_notifications("All")
+
+
+def trigger_whatsapp_notifications_hourly():
+    """Run hourly."""
+    trigger_whatsapp_notifications("Hourly")
+
+
+def trigger_whatsapp_notifications_daily():
+    """Run daily."""
+    trigger_whatsapp_notifications("Daily")
+
+
+def trigger_whatsapp_notifications_weekly():
+    """Trigger notification."""
+    trigger_whatsapp_notifications("Weekly")
+
+
+def trigger_whatsapp_notifications_monthly():
+    """Trigger notification."""
+    trigger_whatsapp_notifications("Monthly")
+
+
+def trigger_whatsapp_notifications_yearly():
+    """Trigger notification."""
+    trigger_whatsapp_notifications("Yearly")
+
+
+def trigger_whatsapp_notifications_hourly_long():
+    """Trigger notification."""
+    trigger_whatsapp_notifications("Hourly Long")
+
+
+def trigger_whatsapp_notifications_daily_long():
+    """Trigger notification."""
+    trigger_whatsapp_notifications("Daily Long")
+
+
+def trigger_whatsapp_notifications_weekly_long():
+    """Trigger notification."""
+    trigger_whatsapp_notifications("Weekly Long")
+
+
+def trigger_whatsapp_notifications_monthly_long():
+    """Trigger notification."""
+    trigger_whatsapp_notifications("Monthly Long")
+
+
+def trigger_whatsapp_notifications(event):
+    """Run cron."""
+    frappe.get_doc(
+        "WhatsApp Notification",
+        frappe.db.get_value("WhatsApp Notification", filters={"event_frequency": event})
+    ).execute_method()
+    pass
