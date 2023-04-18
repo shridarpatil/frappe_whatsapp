648f9b40563da6815ee14fa589f7b631ee54b2b7
Thakur Vinay Rawat <vineyrawat@yahoo.com>
2023-04-18 12:25:31 +0530

    Added changed for both v15 and v16 to handle incoming messages

--- frappe_whatsapp/utils/webhook.py
+++ frappe_whatsapp/utils/webhook.py
@@ -35,7 +35,12 @@ def post():
         "meta_data": json.dumps(data)
     }).insert(ignore_permissions=True)
 
-    messages = data["entry"]["changes"][0]["value"].get("messages", [])
+    messages = []
+    try:
+        messages = data["entry"][0]["changes"][0]["value"].get("messages", [])
+    except KeyError:
+        messages = data["entry"]["changes"][0]["value"].get("messages", [])
+
     if messages:
         for message in messages:
             if message['type'] == 'text':
