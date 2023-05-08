d49140272a4a0b58a91de3f66081147ac1c3b733
emanuelediluzio <emanuelediluzio0@gmail.com>
2023-05-08 17:04:52 +0200

    changes

--- frappe_whatsapp/utils/webhook.py
+++ frappe_whatsapp/utils/webhook.py
@@ -116,15 +116,16 @@ def post():
     if messages:
      for message in messages:
         if message['type'] == 'text':
+            frappe.throw("prova")
             frappe.get_doc({
                 "doctype": "WhatsApp Message",
                 "type": "Incoming",
                 "from": message['from'],
                 "message": message['text']['body']
             }).insert(ignore_permissions=True)
-            customer_name = frappe.db.get_value("Customer", filters={"mobile_no": message['from']}, fieldname="name")
-            frappe.msgprint("Messaggio inviato da {customer_name}: {message['text']['body']}", indicator="green", alert=True)
-            frappe.throw("prova")
+            #customer_name = frappe.db.get_value("Customer", filters={"mobile_no": message['from']}, fieldname="name")
+            #frappe.msgprint("Messaggio inviato da {customer_name}: {message['text']['body']}", indicator="green", alert=True)
+            
     else:
         changes = None
         try:
