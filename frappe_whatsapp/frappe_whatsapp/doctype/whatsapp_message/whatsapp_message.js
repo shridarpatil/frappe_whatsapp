b8c3e6f01295ce8369df3f8ceeada0147f202973
emanuelediluzio <emanuelediluzio0@gmail.com>
2023-05-09 16:13:03 +0200

    changes

--- frappe_whatsapp/frappe_whatsapp/doctype/whatsapp_message/whatsapp_message.js
+++ frappe_whatsapp/frappe_whatsapp/doctype/whatsapp_message/whatsapp_message.js
@@ -2,23 +2,22 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("WhatsApp Message", {
-    refresh: function(frm) {
-        frappe.db.get_list('Customer', { fields: ['name'] })
-            .then(function(result) {
-                var customerNames = result.map(function(item) {
-                    return item.name;
-                });
-                cur_frm.set_df_property("a", "options", customerNames);
-            });
-
-        frm.fields_dict.mode.$input.on("change", function() {
-            if (frm.doc.mode) {
-                frm.set_df_property("a", "read_only", 1);
-                frm.set_df_property("gruppo", "read_only", 0);
-            } else {
-                frm.set_df_property("a", "read_only", 0);
-                frm.set_df_property("gruppo", "read_only", 1);
-            }
-        });
+  refresh: function(frm) {
+      frappe.db.get_list('Customer', { fields: ['name'] })
+          .then(function(result) {
+              var customerNames = result.map(function(item) {
+                  return item.name;
+              });
+              cur_frm.set_df_property("a", "options", customerNames);
+          });
+  },
+  refresh: function(frm) {
+    if (frm.doc.mode) {
+        frm.set_df_property("a", "read_only", 1);
+        frm.set_df_property("gruppo", "read_only", 0);
+    } else {
+        frm.set_df_property("a", "read_only", 0);
+        frm.set_df_property("gruppo", "read_only", 1);
     }
+}
 });
