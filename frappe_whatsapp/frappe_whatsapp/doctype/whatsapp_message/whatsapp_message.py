0c903260b35f179da3e5fcd86cfb4d19ab8943ae
emanuelediluzio <emanuelediluzio0@gmail.com>
2023-05-12 20:12:05 +0200

    implementing/testing notifyAll

--- frappe_whatsapp/frappe_whatsapp/doctype/whatsapp_message/whatsapp_message.py
+++ frappe_whatsapp/frappe_whatsapp/doctype/whatsapp_message/whatsapp_message.py
@@ -29,9 +29,18 @@ class WhatsAppMessage(Document):
                         self.send_message(mobile_no, link)
                         #frappe.msgprint(str(mobile_no), indicator="green", alert=True)
                         time.sleep(2) #"dorme" per mezzo secondo
+
+            if self.notifica:
+                # Invia il template di benvenuto a tutti i numeri degli utenti
+                customers = frappe.db.get_list("Customer", pluck="customer_name")
+                for customer in customers:
+                    mobile_no = frappe.db.get_value("Customer", filters={"customer_name": customer}, fieldname="mobile_no")
+                    if mobile_no:
+                        self.notifyAll(mobile_no)
+                        time.sleep(2) #"dorme" per mezzo secondo            
                         
               
-            else:
+            if not self.switch and not self.notifica:
                 # Invia messaggio al singolo utente nel campo "a"
                 mobile_no = frappe.db.get_value("Customer", filters={"customer_name": self.a}, fieldname="mobile_no")
                 if mobile_no:
@@ -84,6 +93,43 @@ class WhatsAppMessage(Document):
             response = make_post_request(
                 f"{settings.url}/{settings.version}/{settings.phone_id}/messages",
                 headers=headers, data=json.dumps(data)
+            )
+            self.message_id = response['messages'][0]['id']
+            #frappe.msgprint("Messaggio inviato a " + self.a + "(" +str(self.format_number(frappe.db.get_value("Customer", filters={"customer_name": self.a}, fieldname="mobile_no"))) +")", indicator="green", alert=True)
+            
+
+        except Exception as e:
+            res = frappe.flags.integration_request.json()['error']
+            error_message = res.get('Error', res.get("message"))
+            frappe.get_doc({
+                "doctype": "WhatsApp Notification Log",
+                "template": "Text Message",
+                "meta_data": frappe.flags.integration_request.json()
+            }).insert(ignore_permissions=True)
+
+            frappe.throw(
+                msg=error_message,
+                title=res.get("error_user_title", "Error")
+            )
+
+    def notifyAll(self, mobile_no):
+        """Notify."""
+
+        settings = frappe.get_doc(
+            "WhatsApp Settings", "WhatsApp Settings",
+        )
+        token = settings.get_password("token")
+
+        template = '{ \"messaging_product\": \"whatsapp\", \"to\": \"'+str(mobile_no)+'\", \"type\": \"template\", \"template\": { \"name\": \"hello_world\", \"language\": { \"code\": \"en_US\" } } }'
+
+        headers = {
+            "authorization": f"Bearer {token}",
+            "content-type": "application/json"
+        }
+        try:
+            response = make_post_request(
+                f"{settings.url}/{settings.version}/{settings.phone_id}/messages",
+                headers=headers, data=template
             )
             self.message_id = response['messages'][0]['id']
             #frappe.msgprint("Messaggio inviato a " + self.a + "(" +str(self.format_number(frappe.db.get_value("Customer", filters={"customer_name": self.a}, fieldname="mobile_no"))) +")", indicator="green", alert=True)
