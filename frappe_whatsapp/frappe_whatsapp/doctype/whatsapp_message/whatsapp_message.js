456f15968512858d2bd2cac93c901a8200faf856
shridhar <shridhar.p@zerodha.com>
2023-10-19 18:28:35 +0530

    feat: Added reply button for incomming message

--- frappe_whatsapp/frappe_whatsapp/doctype/whatsapp_message/whatsapp_message.js
+++ frappe_whatsapp/frappe_whatsapp/doctype/whatsapp_message/whatsapp_message.js
@@ -2,7 +2,12 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('WhatsApp Message', {
-	// refresh: function(frm) {
+	refresh: function(frm) {
+		if (frm.doc.type == 'Incoming'){
+			frm.add_custom_button(__("Reply"), function(){
+				frappe.new_doc("WhatsApp Message", {"to": frm.doc.from});
 
-	// }
+			});
+		}
+	}
 });
