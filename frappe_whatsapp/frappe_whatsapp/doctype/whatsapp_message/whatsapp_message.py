7efe8560cdc060916cf4735316da760150d83001
Shariq Ansari <sharique.rik@gmail.com>
2024-04-23 15:10:05 +0530

    fix: send template message

--- frappe_whatsapp/frappe_whatsapp/doctype/whatsapp_message/whatsapp_message.py
+++ frappe_whatsapp/frappe_whatsapp/doctype/whatsapp_message/whatsapp_message.py
@@ -11,39 +11,34 @@ class WhatsAppMessage(Document):
 
     def before_insert(self):
         """Send message."""
-        if self.type == 'Outgoing' and self.message_type != 'Template':
+        if self.type == "Outgoing" and self.message_type != "Template":
             if self.attach and not self.attach.startswith("http"):
-                link = frappe.utils.get_url() + '/'+ self.attach
+                link = frappe.utils.get_url() + "/" + self.attach
             else:
                 link = self.attach
 
             data = {
                 "messaging_product": "whatsapp",
                 "to": self.format_number(self.to),
-                "type": self.content_type
+                "type": self.content_type,
             }
             if self.is_reply and self.reply_to_message_id:
                 data["context"] = {"message_id": self.reply_to_message_id}
-            if self.content_type in ['document', 'image', 'video']:
-                 data[self.content_type.lower()] = {
+            if self.content_type in ["document", "image", "video"]:
+                data[self.content_type.lower()] = {
                     "link": link,
-                    "caption": self.message
+                    "caption": self.message,
                 }
             elif self.content_type == "reaction":
                 data["reaction"] = {
                     "message_id": self.reply_to_message_id,
-                    "emoji": self.message
+                    "emoji": self.message,
                 }
             elif self.content_type == "text":
-                data["text"] = {
-                    "preview_url": True,
-                    "body": self.message
-                }
+                data["text"] = {"preview_url": True, "body": self.message}
 
             elif self.content_type == "audio":
-                data["text"] = {
-                    "link": link
-                }
+                data["text"] = {"link": link}
 
             try:
                 self.notify(data)
@@ -51,42 +46,78 @@ class WhatsAppMessage(Document):
             except Exception as e:
                 self.status = "Failed"
                 frappe.throw(f"Failed to send message {str(e)}")
+        elif self.type == "Outgoing" and self.message_type == "Template":
+            self.send_template()
+
+    def send_template(self):
+        """Send template."""
+        template = frappe.get_doc("WhatsApp Templates", self.template)
+        data = {
+            "messaging_product": "whatsapp",
+            "to": self.format_number(self.to),
+            "type": "template",
+            "template": {
+                "name": template.actual_name or template.template_name,
+                "language": {"code": template.language_code},
+                "components": [],
+            },
+        }
+
+        if template.sample_values:
+            field_names = template.sample_values.split(",")
+            parameters = []
+
+            for field_name in field_names:
+                value = frappe.db.get_value(
+                    self.reference_doctype, self.reference_name, field_name
+                )
+                parameters.append({"type": "text", "text": value})
+
+            data["template"]["components"].append(
+                {
+                    "type": "body",
+                    "parameters": parameters,
+                }
+            )
+
+        self.notify(data)
 
     def notify(self, data):
         """Notify."""
         settings = frappe.get_doc(
-            "WhatsApp Settings", "WhatsApp Settings",
+            "WhatsApp Settings",
+            "WhatsApp Settings",
         )
         token = settings.get_password("token")
 
         headers = {
             "authorization": f"Bearer {token}",
-            "content-type": "application/json"
+            "content-type": "application/json",
         }
         try:
             response = make_post_request(
                 f"{settings.url}/{settings.version}/{settings.phone_id}/messages",
-                headers=headers, data=json.dumps(data)
+                headers=headers,
+                data=json.dumps(data),
             )
-            self.message_id = response['messages'][0]['id']
+            self.message_id = response["messages"][0]["id"]
 
         except Exception as e:
-            res = frappe.flags.integration_request.json()['error']
-            error_message = res.get('Error', res.get("message"))
-            frappe.get_doc({
-                "doctype": "WhatsApp Notification Log",
-                "template": "Text Message",
-                "meta_data": frappe.flags.integration_request.json()
-            }).insert(ignore_permissions=True)
+            res = frappe.flags.integration_request.json()["error"]
+            error_message = res.get("Error", res.get("message"))
+            frappe.get_doc(
+                {
+                    "doctype": "WhatsApp Notification Log",
+                    "template": "Text Message",
+                    "meta_data": frappe.flags.integration_request.json(),
+                }
+            ).insert(ignore_permissions=True)
 
-            frappe.throw(
-                msg=error_message,
-                title=res.get("error_user_title", "Error")
-            )
+            frappe.throw(msg=error_message, title=res.get("error_user_title", "Error"))
 
     def format_number(self, number):
         """Format number."""
         if number.startswith("+"):
-            number = number[1:len(number)]
+            number = number[1 : len(number)]
 
         return number
