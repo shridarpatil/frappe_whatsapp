810c18b4b25a46348188736c817b48d6eebab55e
chechani <ca.bc.chechani@gmail.com>
2024-05-27 12:30:39 +0530

    keyword message triggers

---
+++ frappe_whatsapp/frappe_whatsapp/doctype/whatsapp_keyword_message/whatsapp_keyword_message.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Shridhar Patil and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class WhatsAppKeywordMessage(Document):
+	pass
