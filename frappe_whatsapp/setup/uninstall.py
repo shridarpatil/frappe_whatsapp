cdad42d392fa77a9614471ed20f5b59f4665b48e
Eben van Deventer <buff0k@gmail.com>
2024-08-16 15:57:45 +0200

    Update uninstall.py
    Remove WhatsApp Integrations Card before uninstall

--- frappe_whatsapp/setup/uninstall.py
+++ frappe_whatsapp/setup/uninstall.py
@@ -2,17 +2,24 @@ import frappe
 
 def remove_integrations_card():
     try:
+        # Fetch the Integrations workspace document
         workspace = frappe.get_doc('Workspace', 'Integrations')
     except frappe.DoesNotExistError:
         frappe.log_error("The Integrations workspace does not exist.")
         return
-
+    
+    # Find the index of the card break with the label "WhatsApp Integrations"
     card_break_index = next((index for (index, d) in enumerate(workspace.links) if d.type == "Card Break" and d.label == "WhatsApp"), None)
     
     if card_break_index is not None:
+        # Remove the card break and all links after it until the next card break or the end of the list
         workspace.links.pop(card_break_index)
+        
+        # Remove subsequent links associated with the "WhatsApp Integrations" card
         while card_break_index < len(workspace.links) and workspace.links[card_break_index].type == "Link":
             workspace.links.pop(card_break_index)
+        
+        # Save the updated workspace
         workspace.save()
         frappe.db.commit()
         frappe.log_error("WhatsApp Integrations card and links removed successfully.")
