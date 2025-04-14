ffaa446c5d7503ef9d51b4fd58b3d9cff967fe47
nilpatel42 <ndvadalia1@gmail.com>
2025-04-14 11:09:30 +0530

    fix: cron validation

--- frappe_whatsapp/frappe_whatsapp/doctype/whatsapp_settings/whatsapp_settings.py
+++ frappe_whatsapp/frappe_whatsapp/doctype/whatsapp_settings/whatsapp_settings.py
@@ -22,42 +22,38 @@ def validate_cron_format(cron_time):
 
 def update_cron_job(doc):
     """Create or update Scheduled Job Type and Scheduler Event when cron_time changes."""
+    if not doc.cron_time:
+        frappe.msgprint("Cron time not set. Skipping job creation.")
+        return
+
+    # Validate cron before saving job
+    validate_cron_format(doc.cron_time)
+
     scheduled_against_value = "WhatsApp Notification"
     method_value = "frappe_whatsapp.frappe_whatsapp.doctype.whatsapp_notification.whatsapp_notification.trigger_notifications"
 
     # Check if Scheduler Event exists
     sch_event_name = frappe.db.get_value("Scheduler Event", {"scheduled_against": scheduled_against_value}, "name")
     if sch_event_name:
-        # Update existing Scheduler Event
         sch_event = frappe.get_doc("Scheduler Event", sch_event_name)
-        sch_event.method = method_value  # Set the method field
-        sch_event.save(ignore_permissions=True)  # Save to ensure any updates are persisted
+        sch_event.method = method_value
+        sch_event.save(ignore_permissions=True)
     else:
-        # Create new Scheduler Event
         sch_event = frappe.new_doc("Scheduler Event")
         sch_event.scheduled_against = scheduled_against_value
-        sch_event.method = method_value  # Set the method field
+        sch_event.method = method_value
         sch_event.save(ignore_permissions=True)
         sch_event_name = sch_event.name
 
     # Check if Scheduled Job Type exists
-    job_name = frappe.db.get_value(
-        "Scheduled Job Type",
-        {
-            "method": method_value
-        },
-        "name"
-    )
-
+    job_name = frappe.db.get_value("Scheduled Job Type", {"method": method_value}, "name")
     if job_name:
-        # Update existing job
         job = frappe.get_doc("Scheduled Job Type", job_name)
         job.cron_format = doc.cron_time
-        job.scheduler_event = sch_event_name  # Ensure the scheduler event is linked
+        job.scheduler_event = sch_event_name
         job.save(ignore_permissions=True)
         frappe.msgprint("Whatsapp Notification Time Updated Successfully!")
     else:
-        # Create new Scheduled Job Type
         job = frappe.new_doc("Scheduled Job Type")
         job.method = method_value
         job.frequency = "Cron"
