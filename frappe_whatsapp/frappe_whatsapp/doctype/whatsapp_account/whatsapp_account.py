cc5dc5bda937b8f31222146eebef838925677a16
Abraham Kalungi <abrahamskalungi@gmail.com>
2025-05-26 23:09:33 +0200

    feat: make sure there is only one default of each type

--- frappe_whatsapp/frappe_whatsapp/doctype/whatsapp_account/whatsapp_account.py
+++ frappe_whatsapp/frappe_whatsapp/doctype/whatsapp_account/whatsapp_account.py
@@ -1,9 +1,25 @@
 # Copyright (c) 2025, Shridhar Patil and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class WhatsAppAccount(Document):
-	pass
+	def on_update(self):
+		"""Check there is only one default of each type."""
+		self.there_must_be_only_one_default()
+
+	def there_must_be_only_one_default(self):
+		"""If current WhatsApp Account is default, un-default all other accounts."""
+		for field in ("is_default_incoming", "is_default_outgoing"):
+			if not self.get(field):
+				continue
+
+			for whatsapp_account in frappe.get_all("WhatsApp Account", filters={field: 1}):
+				if whatsapp_account.name == self.name:
+					continue
+
+				whatsapp_account = frappe.get_doc("WhatsApp Account", whatsapp_account.name)
+				whatsapp_account.set(field, 0)
+				whatsapp_account.save()
