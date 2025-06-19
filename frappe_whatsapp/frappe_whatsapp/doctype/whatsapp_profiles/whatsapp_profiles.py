c7e6fae9dcac4c3c12fcda8ddbe0c6da958096a4
Kalungi Abraham <abrahamskalungi@gmail.com>
2025-06-19 17:25:44 +0000

    add: whatsapp account on profile doctype

---
+++ frappe_whatsapp/frappe_whatsapp/doctype/whatsapp_profiles/whatsapp_profiles.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Shridhar Patil and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class WhatsAppProfiles(Document):
+	pass
