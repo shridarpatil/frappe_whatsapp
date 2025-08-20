a640dd2c46a3803579b775b6e32a4a1eed6daf42
HEDST <hedst.co@gmail.com>
2025-08-20 15:21:19 +0000

    Update port to 8080 for Kinsta compatibility

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.get('/', (req, res) => {
   res.send('HedstApp başarıyla çalışıyor 🎉');
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = 8080;
 app.listen(PORT, () => {
   console.log(`Sunucu ${PORT} portunda çalışıyor`);
 });
