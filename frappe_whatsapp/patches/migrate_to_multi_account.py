b8dc60878bd8b4ce79007e671c7f4af2319d0d09
Abraham Kalungi <abrahamskalungi@gmail.com>
2025-08-30 07:56:56 +0200

    fix: migrate WhatsApp Settings data into WhatsApp Account

--- frappe_whatsapp/patches/migrate_to_multi_account.py
+++ frappe_whatsapp/patches/migrate_to_multi_account.py
@@ -1,44 +1,50 @@
 import frappe
 
-def field_exists(doctype, fieldname):
-	meta = frappe.get_meta(doctype)
-	return any(df.fieldname == fieldname for df in meta.fields)
-
 def execute():
-	if not field_exists("WhatsApp Settings", "phone_id") or not field_exists("WhatsApp Settings", "token"):
-		return
+    if not frappe.db.table_exists("WhatsApp Settings") or not frappe.db.table_exists("WhatsApp Account"):
+        return
 
-	settings = frappe.get_single("WhatsApp Settings")
+    phone_id = frappe.db.get_single_value("WhatsApp Settings", "phone_id")
+    token = frappe.db.get_single_value("WhatsApp Settings", "token")
+    business_id = frappe.db.get_single_value("WhatsApp Settings", "business_id")
+    app_id = frappe.db.get_single_value("WhatsApp Settings", "app_id")
+    url = frappe.db.get_single_value("WhatsApp Settings", "url")
+    version = frappe.db.get_single_value("WhatsApp Settings", "version")
+    webhook_verify_token = frappe.db.get_single_value("WhatsApp Settings", "webhook_verify_token")
+    enabled = frappe.db.get_single_value("WhatsApp Settings", "enabled")
 
-	existing = frappe.get_all("WhatsApp Account", filters={"phone_id": settings.phone_id})
-	if existing:
-		return
+    if not phone_id or not token:
+        return
 
-	account = frappe.get_doc({
-		"doctype": "WhatsApp Account",
-		"account_name": "Default WhatsApp Account",
-		"phone_id": settings.phone_id,
-		"business_id": settings.business_id,
-		"app_id": settings.app_id,
-		"token": settings.get_password("token"),
-		"url": settings.url,
-		"version": settings.version,
-		"webhook_verify_token": settings.webhook_verify_token,
-		"token": settings.token,
-		"is_default_incoming": 1,
-		"is_default_outgoing": 1,
-		"status": "Active" if settings.enabled == 1 else "Inactive"
-	})
-	account.insert(ignore_permissions=True)
-	settings.save()
-	update_whatsapp_templates(account.name)
-	frappe.db.commit()
+    if frappe.db.exists("WhatsApp Account", {"phone_id": phone_id}):
+        return
 
-def update_whatsapp_templates(account):
-	whatsapp_templates = frappe.get_all(
-		'WhatsApp Templates',
-		filters={'whatsapp_account': ''},
-		fields=['name']
-	)
-	for template in whatsapp_templates:
-		frappe.db.set_value('WhatsApp Templates', template['name'], 'whatsapp_account', account)
+    account = frappe.get_doc({
+        "doctype": "WhatsApp Account",
+        "account_name": "Default WhatsApp Account",
+        "phone_id": phone_id,
+        "business_id": business_id,
+        "app_id": app_id,
+        "token": token,
+        "url": url,
+        "version": version,
+        "webhook_verify_token": webhook_verify_token,
+        "is_default_incoming": 1,
+        "is_default_outgoing": 1,
+        "status": "Active" if enabled == 1 else "Inactive"
+    })
+    account.insert(ignore_permissions=True)
+
+    update_whatsapp_templates(account.name)
+
+    frappe.db.commit()
+
+
+def update_whatsapp_templates(account_name: str):
+    templates = frappe.get_all(
+        "WhatsApp Templates",
+        filters={"whatsapp_account": ""},
+        fields=["name"]
+    )
+    for template in templates:
+        frappe.db.set_value("WhatsApp Templates", template["name"], "whatsapp_account", account_name)
