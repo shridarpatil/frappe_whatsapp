1b34ac905ccd1af1168e49f30370c0fdc4b1dbcc
Shridhar Patil <shridharpatil2792@gmail.com>
2026-04-20 19:33:16 +0530

    feat: add Subscribe App to Webhooks action on WhatsApp Account (#211)
    Calls POST /{version}/{business_id}/subscribed_apps on the Graph API so
    that the WABA starts delivering incoming messages to the configured
    webhook after phone number registration.

--- frappe_whatsapp/frappe_whatsapp/doctype/whatsapp_account/whatsapp_account.js
+++ frappe_whatsapp/frappe_whatsapp/doctype/whatsapp_account/whatsapp_account.js
@@ -1,8 +1,32 @@
 // Copyright (c) 2025, Shridhar Patil and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("WhatsApp Account", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("WhatsApp Account", {
+	refresh(frm) {
+		if (!frm.is_new()) {
+			frm.add_custom_button(__("Subscribe App to Webhooks"), () => {
+				frappe.confirm(
+					__("Subscribe this app to webhooks for WhatsApp Business Account {0}?", [
+						frm.doc.business_id || frm.doc.account_name,
+					]),
+					() => {
+						frm.call({
+							doc: frm.doc,
+							method: "subscribe_app",
+							freeze: true,
+							freeze_message: __("Subscribing app to webhooks..."),
+							callback: (r) => {
+								if (!r.exc) {
+									frappe.show_alert({
+										message: __("App subscribed to webhooks"),
+										indicator: "green",
+									});
+								}
+							},
+						});
+					}
+				);
+			});
+		}
+	},
+});
